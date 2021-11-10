ab64f9573cb0c818c65c19d6fc8683d6406478a6
ho-joon-kim <hojoon.dev@gmail.com>
2021-11-11 04:06:09 +0900

    add - Lyrics

--- src/Components/app/page/Lyrics.js
+++ src/Components/app/page/Lyrics.js
@@ -1,14 +1,19 @@
 import { Box } from "grommet";
 import { Update, Download, Close } from 'grommet-icons'
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios'
 
 import ServiceLayout from "../Layout";
+import * as configUrl from "../../../config";
 
 const Lyrics = () => {
   const History = useHistory();
+  const [title, SetTitle] = useState('');
+  const [story, SetStory] = useState(['','','']);
+  const [content, SetOutputContent] = useState('')
 
   useEffect(() => {
     const loginCheck = localStorage.getItem("token");
@@ -20,6 +25,29 @@ const Lyrics = () => {
       setTimeout(toast.info("로그인을 해주세요!"), 300);
     }
   }, []);
+
+  const LyricsAxios = async () => {
+    SetOutputContent('');
+    if (title !== '' && story) {
+      const config = {
+        method: 'post',
+        url: `${configUrl.SERVER_URL}/writinggel/lyrics`,
+        headers: { 'authentication': localStorage.getItem("token"), },
+        data : { title, story }
+      };
+
+      await axios(config)
+      .then(async (response) => {
+        console.log(response.data);
+        SetOutputContent(response.data[0]);
+      })
+      .catch(async (error) => {
+        console.log(error);
+      });
+    } else {
+      setTimeout(toast.info("내용을 채워주세요!"), 300);
+    }
+  };
 
   return (
     <ServiceLayout>
@@ -34,18 +62,18 @@ const Lyrics = () => {
         <Box className='LyricInputBox'>
           <div className='subjectTitle'>
             <p>제목<span style={{color:'red'}}>*</span></p>
-            <input type='text' placeholder='제목을 적어주세요!' required />
+            <input type='text' placeholder='제목을 적어주세요!' required onChange={(e)=>{SetTitle(e.target.value)}}/>
           </div>
           <div className='subjects'>
             <p>주제(3개 입력)<span style={{color:'red'}}>*</span></p>
-            <input type='text' placeholder='주제를 적어주세요!' required />
-            <input type='text' placeholder='주제를 적어주세요!' required />
-            <input type='text' placeholder='주제를 적어주세요!' required />
+            <input type='text' placeholder='주제를 적어주세요!' required onChange={(e)=>{SetStory([e.target.value,story[1],story[2]])}}/>
+            <input type='text' placeholder='주제를 적어주세요!' required onChange={(e)=>{SetStory([story[0],e.target.value,story[2]])}}/>
+            <input type='text' placeholder='주제를 적어주세요!' required onChange={(e)=>{SetStory([story[0],story[1],e.target.value])}}/>
           </div>
-          <button>영어 가사 쓰기</button>
+          <button onClick = {()=>{LyricsAxios();}}>영어 가사 쓰기</button>
         </Box>
         <Box className='LyricOutputBox'>
-            <textarea />
+            <textarea value={content}/>
             <div className='icons'> <div><Close />  <Update/> <Download/> </div> </div>
         </Box>
       </Box>
