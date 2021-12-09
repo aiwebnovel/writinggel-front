00506d9e0cdaf8f0499a65350a80dda595e70e75
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-12-09 14:39:34 +0900

    #88 무료체험하기 버튼 추가

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,52 +1,269 @@
-import React, {useContext } from "react";
-import { Link } from 'react-router-dom'
+import React, { useContext, useState, useCallback } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 import Layout from "./Layout";
-import { Box, Grid, Card, CardBody, CardHeader, ResponsiveContext} from "grommet";
-import { Magic} from "grommet-icons";
+import {
+  Box,
+  Grid,
+  Card,
+  CardBody,
+  CardHeader,
+  ResponsiveContext,
+} from "grommet";
+import { User, Menu, Google, FacebookOption, Down } from "grommet-icons";
+import ScrollToTop from "../routes/ScrollToTop";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Modal from "./Modal";
+import * as config from "../config";
+
+import { authService, firebaseInstance } from "../firebaseConfig";
+import LinkObject from "./app/LinkObject";
 import "../styles/Main.scss";
 
-import LinkObject from "./app/LinkObject";
-
 const Home = () => {
-    
-  const size = useContext(ResponsiveContext)
-  //const History = useHistory();
-  
-    // useEffect(() => {
-    //   const loginCheck = localStorage.getItem("token");
-  
-    //   if (loginCheck !== null) {
-    //     return;
-    //   } else {
-    //    toast.info("로그인을 해주세요!");
-    //   }
-    // }, [History]);
+  const [isChecked, SetChecked] = useState(false);
+  const [isOpen, SetOpen] = useState(false);
+  const [profile, SetProfile] = useState({
+    userName: "Guest",
+    userImage: `User`,
+    isBill: "",
+    Plan: "",
+  });
+
+  const size = useContext(ResponsiveContext);
+  const history = useHistory();
+
+  const HandleModals = () => {
+    let loginCheck = localStorage.getItem('token');
+    if(!loginCheck) {
+      SetOpen(!isOpen);
+    }else {
+      toast.warn('이미 로그인 되어있네요!');
+    }
+ 
+  };
+
+  const HandleChecked = (e) => {
+    SetChecked(e.target.checked);
+  };
+
+  const signIn = async (e) => {
+    if (isChecked === true) {
+      let name = e.target.name;
+      // let provider = new firebaseInstance.auth.GoogleAuthProvider();
+      if (name === "Facebook") {
+        let provider = new firebaseInstance.auth.FacebookAuthProvider();
+        //await authService.signInWithRedirect(provider)
+        await authService
+          .signInWithPopup(provider)
+          .then(async (dataFacebook) => {
+            console.log(dataFacebook);
+            //const credential = FacebookAuthProvider.credentialFromResult(dataFacebook);
+            //console.log('cre', credential);
+
+            let credentials = dataFacebook.credential;
+            //let id = dataFacebook.credential.providerId //facebook.com
+            let email = dataFacebook.user.email;
+            let create = dataFacebook.user.metadata.creationTime;
+            let token = credentials.accessToken;
+            //console.log('result',credentials, email,create,token, id);
+
+            await localStorage.setItem("token", token);
+            await localStorage.setItem("email", email);
+            await localStorage.setItem("create", create);
+
+            await requestProfile();
+            await SetOpen(false);
+
+            refreshProfile();
+            history.replace('/app/firstsentence');
+          })
+          .catch((error) => {
+            console.log(error);
+            if (
+              error.code === "auth/account-exists-with-different-credential"
+            ) {
+              toast.error(
+                "이미 구글로 로그인했던 계정입니다. 동일한 이메일 주소를 사용하여 여러 계정을 만들 수 없습니다."
+              );
+            }
+          });
+      } else if (name === "Google") {
+        let provider = new firebaseInstance.auth.GoogleAuthProvider();
+        //await authService.signInWithRedirect(provider)
+        await authService
+          .signInWithPopup(provider)
+          .then(async (dataGoogle) => {
+            console.log(dataGoogle);
+
+            let credential = dataGoogle.credential;
+            let email = dataGoogle.user.email;
+            let create = dataGoogle.user.metadata.creationTime;
+            let token = credential.idToken;
+            console.log("result", credential, email, create, token);
+
+            await localStorage.setItem("token", token);
+            await localStorage.setItem("email", email);
+            await localStorage.setItem("create", create);
+
+            await requestProfile();
+            await SetOpen(false);
+
+            refreshProfile();
+            history.replace('/app/firstsentence');
+          })
+          .catch((error) => {
+            console.log(error);
+            if (
+              error.code === "auth/account-exists-with-different-credential"
+            ) {
+              toast.error(
+                "이미 페이스북으로 로그인했던 계정입니다. 동일한 이메일 주소를 사용하여 여러 계정을 만들 수 없습니다."
+              );
+            }
+          });
+      }
+    } else {
+      toast.error("이용약관 및 개인정보처리방침에 동의해주세요!");
+    }
+  };
+
+  const requestProfile = useCallback(async () => {
+    if (localStorage.getItem("token") !== null) {
+      await axios
+        .get(`${config.SERVER_URL}/profile`, {
+          headers: { authentication: localStorage.getItem("token") },
+        })
+        .then((response) => {
+          SetProfile({
+            ...profile,
+            userName: response.data.name,
+            userImage: response.data.photoURL,
+            isBill: response.data.isBill,
+            Plan: response.data.plan,
+          });
+
+          localStorage.setItem("userUid", response.data.uid);
+          localStorage.setItem("plan", response.data.plan);
+          localStorage.setItem("isBill", response.data.isBill);
+          localStorage.setItem("userName", response.data.name);
+          localStorage.setItem("userImage", response.data.photoURL);
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response.status === 412) {
+            toast.error("새로고침하거나 다시 로그인 해주세요!");
+          }
+        });
+    }
+  }, [profile]);
+
+  const refreshProfile = useCallback(async () => {
+    authService.onAuthStateChanged(async (user) => {
+      if (authService.currentUser) {
+        authService.currentUser
+          .getIdToken()
+          .then(async (data) => {
+            await localStorage.setItem("token", data);
+          })
+          .catch(async (error) => {
+            console.log(error);
+          });
+      }
+    });
+  }, []);
 
   return (
-    <Layout>
-      <Box 
-      justify="center"
-      align="center"
-      className="MainHome"
+    <>
+      <Layout>
+      <ScrollToTop/>
+        <Box justify='center' align='center' className='MainHome'>
+          <Grid
+            columns={size !== "small" ? { count: 4, size: "auto" } : "100%"}
+            gap='medium'
+            fill={size !== "small" ? false : true}
+          >
+            <Card background='#372874' height='small' onClick={HandleModals} style={{cursor:"pointer", color:'#fff'}}> 
+              <CardBody className='cardTitle' justify='center'>
+                <p>무료체험 해보기</p>
+                <p>(15회)</p>
+              </CardBody>
+            </Card>
+            {LinkObject.map((item) => (
+              <Link to={item.detail || "/"} key={item.id}>
+                <Card background='#f9f9f9' height='small' style={{cursor:"pointer"}}>
+                  <CardHeader className='cardTop'>
+                    {/* <Magic color='#fff' /> */}
+                  </CardHeader>
+                  <CardBody className='cardTitle' justify='center'>
+                    {item.title}
+                  </CardBody>
+                </Card>
+              </Link>
+            ))}
+          </Grid>
+        </Box>
+      </Layout>
+      <Modal
+        onClick={HandleModals}
+        open={isOpen}
+        close={HandleModals}
+        title='Login'
       >
-        <Grid
-          columns={ size !== "small" ? { count: 4, size: "auto" } : '100%'}
-          gap='medium'
-          fill={size !== 'small' ? false : true}
-        >
-          {LinkObject.map((item)=>(
-            <Link to={item.detail || '/'} key={item.id}>
-             <Card background='#f9f9f9' height='small'>
-               <CardHeader className='cardTop'>
-                 <Magic color='#fff' />
-               </CardHeader>
-               <CardBody className='cardTitle' justify='center'>{item.title}</CardBody>
-             </Card>
-           </Link>
-          ))}
-        </Grid>
-      </Box>
-    </Layout>
+        <div className='AvatarBox'>
+          <img src='/user.png' alt='singinUser' className='loginAvatar' />
+        </div>
+
+        <div className='signBox'>
+          <button
+            className='googleButton'
+            name='Google'
+            onClick={(e) => signIn(e)}
+          >
+            <Google color='plain' size='medium' /> Sign in with Google
+          </button>
+
+          <div className='signBox'>
+            <button
+              className='facebookButton'
+              name='Facebook'
+              onClick={(e) => signIn(e)}
+            >
+              <FacebookOption color='plain' size='medium' /> Sign in with
+              Facebook
+            </button>
+          </div>
+          <div className='isChecked'>
+            <input
+              type='checkbox'
+              name='agree'
+              onClick={(e) => {
+                HandleChecked(e);
+              }}
+              style={{ width: "18px", height: "18px", marginRight: "5px" }}
+            />
+            <a
+              href='https://appplatform.notion.site/8be8232fff0341799cf8c13728610b6b'
+              target='_blank'
+              rel='noreferrer'
+            >
+              이용약관
+            </a>
+            과 &nbsp;
+            <a
+              href='https://www.notion.so/appplatform/d99f247a66d141bbbdf227739861a0a2'
+              target='_blank'
+              rel='noreferrer'
+            >
+              개인정보처리방침
+            </a>
+            에&nbsp;동의합니다.
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
